6523079234ef1dcba2988bc5e20b3b36dd71b511
Shalin Dileesha <135975902+dileesha-shalin@users.noreply.github.com>
2024-05-23 10:42:20 +0530

    Add files via upload

---
+++ department-service/src/main/java/com/microservices/departmentservice/entity/Department.java
@@ -0,0 +1,22 @@
+package com.microservices.departmentservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+@Entity
+@Table(name = "department")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String departmentName;
+    private String departmentDescription;
+    private String departmentCode;
+}
---
+++ employee-service/src/main/java/com/microservice/employeeservice/entity/Employee.java
@@ -0,0 +1,23 @@
+package com.microservice.employeeservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstname;
+    private String lastname;
+    @Column(nullable = false, unique = true)
+    private String email;
+    private String departmentCode;
+}
